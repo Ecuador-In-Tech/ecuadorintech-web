b01ce07a06c66f249df898d0fd80a2d58b1ff20a
Brayen Gavilanes <bjgavilanes@gmail.com>
2024-10-30 05:24:01 -0300

    chore(src): remove blog at header

--- src/navigation.ts
+++ src/navigation.ts
@@ -7,10 +7,6 @@ export const headerData = {
       href: getPermalink('/'),
     },
     {
-      text: 'Blog',
-      href: getBlogPermalink(),
-    },
-    {
       text: 'Comunidades',
       href: getPermalink('/'),
     },
